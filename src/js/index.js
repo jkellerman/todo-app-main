3ea1d11c6397c92ca97c446817193eacdd2d4b7e
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-18 13:34:22 +0100

    turn off autocomplete input for chrome

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
 import "../styles/main.scss";
+
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const list = document.querySelector("#list");
+const template = document.querySelector("#list-item-template");
+
+// Add Todos
+// User will type in todo and submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoName = todoInput.value;
+  if (todoName === "") return;
+  renderTodo(todoName);
+  todoInput.value = "";
+});
+
+// Delete Todos
+// Complete Todos
+// Save Todos
+// Load Todos
+
+function renderTodo(todoName) {
+  const templateClone = template.content.cloneNode(true);
+  const textElement = templateClone.querySelector("[data-list-item-text]");
+  textElement.innerText = todoName;
+  list.appendChild(templateClone);
+}
