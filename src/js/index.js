7775510c62332a131072740e62b0d12b33bf28a8
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-19 10:23:00 +0100

    save default todos to local storage, load and checked status of default todos on page load

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,45 @@
 import "../styles/main.scss";
-
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const list = document.querySelector("#list");
+const listItems = Array.from(list.children);
 const template = document.querySelector("#list-item-template");
 const LOCAL_STORAGE_PREFIX = "FE_TODO_LIST";
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
 let todos = loadTodos();
 todos.forEach((todo) => renderTodo(todo));
+const DEFAULT_TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-default-todos`;
+let defaultTodos = loadDefaultTodos();
+defaultTodos.forEach((todo) => renderDefaultTodo(todo));
 
 list.addEventListener("change", (e) => {
   if (!e.target.matches(["[data-list-item-checkbox]"])) return;
-
-  //   Get the todo that is checked
+  if (listItems.includes(e.target.closest(".list-item"))) return;
+  // Get the todo that is checked
   const parent = e.target.closest(".list-item");
   const todoId = parent.dataset.todoId;
   const todo = todos.find((t) => t.id === todoId);
   todo.complete = e.target.checked;
   saveTodos();
+});
+
+list.addEventListener("change", (e) => {
+  if (!e.target.matches(["[data-list-item-checkbox]"])) return;
+  if (!listItems.includes(e.target.closest(".list-item"))) return;
+
+  //   add default checked to local storage
+  const parent = e.target.closest(".list-item");
+  const defaultTodo = {
+    name: parent.innerText,
+    complete: false,
+    id: parent.dataset.todoId,
+  };
+  defaultTodos = defaultTodos.filter((todo) => todo.id !== defaultTodo.id);
+  defaultTodos.push(defaultTodo);
+  const defaultTodoId = defaultTodo.id;
+  const checkedDefault = defaultTodos.find((t) => t.id === defaultTodoId);
+  checkedDefault.complete = e.target.checked;
+  saveDefaultTodos();
 });
 
 list.addEventListener("click", (e) => {
@@ -48,9 +70,6 @@ form.addEventListener("submit", (e) => {
   todoInput.value = "";
 });
 
-// Delete Todos
-// Complete Todos
-
 function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true);
   const listItem = templateClone.querySelector(".list-item");
@@ -62,11 +81,27 @@ function renderTodo(todo) {
   list.appendChild(templateClone);
 }
 
+function renderDefaultTodo(todo) {
+  const defaultTodoId = todo.id;
+  const listItem = listItems.find(
+    (item) => item.dataset.todoId === defaultTodoId
+  );
+  const listItemChild = listItem.firstElementChild.firstElementChild;
+  listItemChild.checked = todo.complete;
+}
+
 function saveTodos() {
   localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+}
+function saveDefaultTodos() {
+  localStorage.setItem(DEFAULT_TODOS_STORAGE_KEY, JSON.stringify(defaultTodos));
 }
 
 function loadTodos() {
   const todosString = localStorage.getItem(TODOS_STORAGE_KEY);
+  return JSON.parse(todosString) || [];
+}
+function loadDefaultTodos() {
+  const todosString = localStorage.getItem(DEFAULT_TODOS_STORAGE_KEY);
   return JSON.parse(todosString) || [];
 }
