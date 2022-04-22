23292043e4b35c06d17150651d4f360ffde757a8
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-22 23:56:25 +0100

    revamp code to make the functionality more precise and cleaner

--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,67 @@
 import "../styles/main.scss";
+import { sampleTodos } from "./sampletodos";
+
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
-const list = document.querySelector("#list");
-let listItems = document.querySelectorAll(".list-item");
-const template = document.querySelector("#list-item-template");
+const list = document.getElementById("list");
 const LOCAL_STORAGE_PREFIX = "FE_TODO_LIST";
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
-const DEFAULT_TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-default-todos`;
-const REMOVED_DEFAULT_TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-removed-default-todos`;
-const ITEMS_LEFT_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-items-left-total`;
-const REORDERED_LIST_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-reordered-list`;
 const all = document.querySelectorAll(".all-btn");
 const active = document.querySelectorAll(".active-btn");
 const completed = document.querySelectorAll(".completed-btn");
 const clear = document.querySelector("#clear");
 const itemsLeft = document.querySelector("[data-items-left]");
 const toggle = document.querySelector(".toggle");
-let newList = list.children;
+// Store listItems
+const listItems = [];
 
-// ======== On reload
-let reorderedList = loadReorderedList();
-if (reorderedList.length > 0) {
-  listItems.forEach((item) => {
-    item.remove();
-  });
-}
-reorderedList.forEach((todo) => renderTodo(todo));
 let todos = loadTodos();
-todos.forEach((todo) => renderTodo(todo));
-let defaultTodos = loadDefaultTodos();
-defaultTodos.forEach((todo) => renderDefaultTodo(todo));
-let removedDefaultTodos = loadRemovedDefaultTodos();
-removedDefaultTodos.forEach((todo) => {
-  const removedDefaultTodoId = todo.id;
-  const listItem = [...listItems].find(
-    (item) => item.dataset.todoId === removedDefaultTodoId
-  );
-  listItem.remove();
-});
-let newTotal = loadItemsTotal();
-itemsLeft.innerText = `${newTotal} items left`;
-
-// ===========
-
-list.addEventListener("change", (e) => {
-  if (!e.target.matches(["[data-list-item-checkbox]"])) return;
-  if ([...listItems].includes(e.target.closest(".list-item"))) return;
-  // Get the todo that is checked
-  const parent = e.target.closest(".list-item");
-  const todoId = parent.dataset.todoId;
-  const todo = todos.find((t) => t.id === todoId);
-  todo.complete = e.target.checked;
+todos.forEach((todo) => {
+  createTodo(todo);
   saveTodos();
 });
 
-list.addEventListener("change", (e) => {
-  if (!e.target.matches(["[data-list-item-checkbox]"])) return;
-  if (![...listItems].includes(e.target.closest(".list-item"))) return;
+// items left
 
-  //   add default todos to local storage
-  const parent = e.target.closest(".list-item");
-  const defaultTodo = {
-    name: parent.innerText,
+itemsLeft.innerText = `${todos.length} items left`;
+
+// Submit new todo
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let listTotal = list.children.length;
+  const todoName = todoInput.value;
+  if (todoName === "") return;
+  const newTodo = {
+    text: todoName,
     complete: false,
-    id: parent.dataset.todoId,
+    id: new Date().valueOf().toString(),
   };
-  defaultTodos = defaultTodos.filter((todo) => todo.id !== defaultTodo.id);
-  defaultTodos.push(defaultTodo);
-  const defaultTodoId = defaultTodo.id;
-  const checkedDefault = defaultTodos.find((t) => t.id === defaultTodoId);
-  checkedDefault.complete = e.target.checked;
-  saveDefaultTodos();
+  todos.push(newTodo);
+  saveTodos(); //   save to local storage
+  todoInput.value = "";
+  listTotal++;
+  itemsLeft.innerText = `${listTotal} items left`;
+  //   Add new todo to DOM
+  createTodo(newTodo);
 });
 
+// detect change when checkbox is checked
+
+list.addEventListener("change", (e) => {
+  if (!e.target.matches("[data-list-item-checkbox]")) return;
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+  const todo = todos.find((t) => t.id === todoId);
+  const item = listItems.find((t) => t.dataset.todoId === todoId);
+  todo.complete = item.firstElementChild.firstElementChild.checked;
+  saveTodos();
+});
+
+//  Delete items when button is clicked
+
 list.addEventListener("click", (e) => {
-  if (!e.target.matches("[data-button-delete]")) return;
-  if ([...listItems].includes(e.target.closest(".list-item"))) return;
-  let listTotal = document.querySelector("#list").children.length;
+  if (!e.target.matches(".delete-btn")) return;
+  let listTotal = list.children.length;
   const parent = e.target.closest(".list-item");
   const todoId = parent.dataset.todoId;
   // remove todo from list
@@ -84,64 +69,34 @@ list.addEventListener("click", (e) => {
   // remove todo from localstorage
   todos = todos.filter((todo) => todo.id !== todoId);
   saveTodos();
-  //   update items left
+  //   decrease items left
   listTotal--;
   itemsLeft.innerText = `${listTotal} items left`;
-  newTotal.splice(0, 1, listTotal);
-  saveItemsTotal();
 });
 
-list.addEventListener("click", (e) => {
-  if (!e.target.matches("[data-button-delete]")) return;
-  if (![...listItems].includes(e.target.closest(".list-item"))) return;
-  let listTotal = document.querySelector("#list").children.length;
-  const parent = e.target.closest(".list-item");
-  const removedDefaultTodo = {
-    name: parent.innerText,
-    complete: false,
-    id: parent.dataset.todoId,
-  };
-  // remove todo from list
-  parent.remove();
-  // add to removed list in localstorage
-  removedDefaultTodos.push(removedDefaultTodo);
-  saveRemovedDefaultTodos();
-  //   update items left
-  listTotal--;
-  itemsLeft.innerText = `${listTotal} items left`;
-  newTotal.splice(0, 1, listTotal);
-  saveItemsTotal();
-});
+// clear completed button is clicked remove items
 
-// submit new todo
+clear.addEventListener("click", () => {
+  const newList = [...list.children];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const todoName = todoInput.value;
-  let listTotal = document.querySelector("#list").children.length;
-  if (todoName === "") return;
-  const newTodo = {
-    name: todoName,
-    complete: false,
-    id: new Date().valueOf().toString(),
-  };
-  todos.push(newTodo);
-  renderTodo(newTodo);
-  saveTodos();
-  todoInput.value = "";
-  //   update items left
-  listTotal++;
-  itemsLeft.innerText = `${listTotal} items left`;
-  newTotal.splice(0, 1, listTotal);
-  saveItemsTotal();
+  let listTotal = list.children.length;
+  newList.forEach((item) => {
+    if (item.firstElementChild.firstElementChild.checked == true) {
+      const itemId = item.dataset.todoId;
+      item.remove();
+      todos = todos.filter((todo) => todo.id !== itemId);
+      saveTodos();
+      listTotal--;
+      itemsLeft.innerText = `${listTotal} items left`;
+    }
+  });
 });
 
 // When all button is clicked, show all items
 
 all.forEach((element) => {
   element.addEventListener("click", () => {
-    const newList = [...document.querySelector("#list").children];
-
+    const newList = [...list.children];
     newList.forEach((item) => {
       item.style.display = "flex";
     });
@@ -155,8 +110,7 @@ all.forEach((element) => {
 
 active.forEach((element) => {
   element.addEventListener("click", () => {
-    const newList = [...document.querySelector("#list").children];
-
+    const newList = [...list.children];
     newList.forEach((item) => {
       if (item.firstElementChild.firstElementChild.checked == true) {
         item.style.display = "none";
@@ -169,13 +123,10 @@ active.forEach((element) => {
     });
   });
 });
-
-// When completed button is clicked show completed items
 
 completed.forEach((element) => {
   element.addEventListener("click", () => {
-    const newList = [...document.querySelector("#list").children];
-
+    const newList = [...list.children];
     newList.forEach((item) => {
       if (item.firstElementChild.firstElementChild.checked == true) {
         item.style.display = "flex";
@@ -189,50 +140,7 @@ completed.forEach((element) => {
   });
 });
 
-// clear completed button is clicked remove items
-
-clear.addEventListener("click", () => {
-  const newList = [...document.querySelector("#list").children];
-
-  let listTotal = document.querySelector("#list").children.length;
-  newList.forEach((item) => {
-    if ([...listItems].includes(item.closest(".list-item"))) return;
-    if (item.firstElementChild.firstElementChild.checked == true) {
-      const itemId = item.dataset.todoId;
-      item.remove();
-      todos = todos.filter((todo) => todo.id !== itemId);
-      saveTodos();
-      listTotal--;
-      itemsLeft.innerText = `${listTotal} items left`;
-      newTotal.splice(0, 1, listTotal);
-      saveItemsTotal();
-    }
-  });
-});
-
-clear.addEventListener("click", () => {
-  const newList = [...document.querySelector("#list").children];
-  let listTotal = document.querySelector("#list").children.length;
-  newList.forEach((item) => {
-    if (![...listItems].includes(item.closest(".list-item"))) return;
-    if (item.firstElementChild.firstElementChild.checked == true) {
-      const removedDefaultTodo = {
-        name: item.innerText,
-        complete: false,
-        id: item.dataset.todoId,
-      };
-      item.remove();
-      removedDefaultTodos.push(removedDefaultTodo);
-      saveRemovedDefaultTodos();
-      listTotal--;
-      itemsLeft.innerText = `${listTotal} items left`;
-      newTotal.splice(0, 1, listTotal);
-      saveItemsTotal();
-    }
-  });
-});
-
-// Theme switcher
+// Switch theme
 
 toggle.addEventListener("click", () => {
   const body = document.querySelector("body");
@@ -247,128 +155,35 @@ toggle.addEventListener("click", () => {
   }
 });
 
-// functions
+// Insert listItems into DOM
 
-function renderTodo(todo) {
-  const templateClone = template.content.cloneNode(true);
-  const listItem = templateClone.querySelector(".list-item");
-  listItem.dataset.todoId = todo.id;
-  const textElement = templateClone.querySelector("[data-list-item-text]");
-  textElement.innerText = todo.name;
-  const checkbox = templateClone.querySelector("[data-list-item-checkbox]");
-  checkbox.checked = todo.complete;
-  list.appendChild(templateClone);
-}
-
-function renderDefaultTodo(todo) {
-  const defaultTodoId = todo.id;
-  const list = [...listItems];
-  const listItem = list.find((item) => item.dataset.todoId === defaultTodoId);
-  const listItemChild = listItem.firstElementChild.firstElementChild;
-  listItemChild.checked = todo.complete;
+function createTodo(todo) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("list-item");
+  listItem.setAttribute("draggable", true);
+  listItem.setAttribute("data-todo-id", todo.id);
+  listItem.innerHTML = `
+          <label class="list-item-label">
+            <input type="checkbox" data-list-item-checkbox />
+            <span class="list-item-text" data-list-item-text
+              >${todo.text}</span
+            >
+          </label>
+          <button class="delete-btn">
+          </button>`;
+  listItems.push(listItem);
+  list.appendChild(listItem);
+  listItem.firstElementChild.firstElementChild.checked = todo.complete;
 }
 
 function saveTodos() {
   localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
 }
-function saveDefaultTodos() {
-  localStorage.setItem(DEFAULT_TODOS_STORAGE_KEY, JSON.stringify(defaultTodos));
-}
-
-function saveRemovedDefaultTodos() {
-  localStorage.setItem(
-    REMOVED_DEFAULT_TODOS_STORAGE_KEY,
-    JSON.stringify(removedDefaultTodos)
-  );
-}
-
-function saveItemsTotal() {
-  localStorage.setItem(ITEMS_LEFT_STORAGE_KEY, JSON.stringify(newTotal));
-}
-
-function saveReorderedList() {
-  localStorage.setItem(
-    REORDERED_LIST_STORAGE_KEY,
-    JSON.stringify(reorderedList)
-  );
-}
 
 function loadTodos() {
   const todosString = localStorage.getItem(TODOS_STORAGE_KEY);
-  return JSON.parse(todosString) || [];
+  return JSON.parse(todosString) || [...sampleTodos];
 }
-function loadDefaultTodos() {
-  const todosString = localStorage.getItem(DEFAULT_TODOS_STORAGE_KEY);
-  return JSON.parse(todosString) || [];
-}
-function loadRemovedDefaultTodos() {
-  const todosString = localStorage.getItem(REMOVED_DEFAULT_TODOS_STORAGE_KEY);
-  return JSON.parse(todosString) || [];
-}
-
-function loadItemsTotal() {
-  const todosString = localStorage.getItem(ITEMS_LEFT_STORAGE_KEY);
-  return JSON.parse(todosString) || [6];
-}
-function loadReorderedList() {
-  const todosString = localStorage.getItem(REORDERED_LIST_STORAGE_KEY);
-  return JSON.parse(todosString) || [];
-}
-
-[...newList].forEach((draggable) => {
-  draggable.addEventListener("dragstart", () => {
-    draggable.classList.add("dragging");
-  });
-  draggable.addEventListener("dragend", () => {
-    draggable.classList.remove("dragging");
-    const newList = [...list.children];
-    draggable.classList.remove("dragging");
-    reorderedList = [];
-    newList.forEach((item) => {
-      const reorderedTodo = {
-        name: item.innerText,
-        complete: item.firstElementChild.firstElementChild.checked,
-        id: item.dataset.todoId,
-      };
-      reorderedList.push(reorderedTodo);
-      saveReorderedList();
-    });
-  });
-});
-
-list.addEventListener("dragover", (e) => {
-  e.preventDefault(); //allow to drop inside container
-  const afterElement = getDragAfterElement(list, e.clientY);
-  const draggable = document.querySelector(".dragging");
-  list.insertBefore(draggable, afterElement);
-});
-
-function saveReorderedList() {
-  localStorage.setItem(
-    REORDERED_LIST_STORAGE_KEY,
-    JSON.stringify(reorderedList)
-  );
-}
-
-function loadReorderedList() {
-  const todosString = localStorage.getItem(REORDERED_LIST_STORAGE_KEY);
-  return JSON.parse(todosString) || [];
-}
-
-function getDragAfterElement(container, y) {
-  const draggableElements = [
-    ...container.querySelectorAll(".list-item:not(.dragging)"),
-  ];
-  return draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-      } else {
-        return closest;
-      }
-    },
-    { offset: Number.NEGATIVE_INFINITY }
-  ).element;
+function removeTodos() {
+  const todosString = localStorage.removeItem(TODOS_STORAGE_KEY);
 }
---
+++ src/js/sampleTodos.js
@@ -0,0 +1,34 @@
+const sampleTodos = [
+  {
+    text: "Complete online JavaScript course",
+    complete: true,
+    id: "0",
+  },
+  {
+    text: "Jog around the park 3x",
+    complete: false,
+    id: "1",
+  },
+  {
+    text: "10 minutes meditation",
+    complete: false,
+    id: "2",
+  },
+  {
+    text: "Read for 1 hour",
+    complete: false,
+    id: "3",
+  },
+  {
+    text: "Pick up groceries",
+    complete: false,
+    id: "4",
+  },
+  {
+    text: "Complete Todo App on Frontend Mentor",
+    complete: false,
+    id: "5",
+  },
+];
+
+export { sampleTodos };
